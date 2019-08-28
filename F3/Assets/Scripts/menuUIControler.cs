8412fcc480bd32294a0708c60af4bc2fe361769b
李佳澳 <52514474+LJA8023@users.noreply.github.com>
2019-08-28 10:36:37 +0800

    UI界面制作1.0
    手柄 图标 退出键 等功能的初步制作

---
+++ F3/Assets/Scripts/menuUIControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuUIControler : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play_Click()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Exit_Click()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
