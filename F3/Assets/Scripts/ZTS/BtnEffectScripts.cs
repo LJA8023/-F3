e6d38383f487a2575e4f9e17cc10fbc9be73bd57
赵田胜 <52514396+muchenwusuoweiju@users.noreply.github.com>
2019-08-28 17:25:41 +0800

    8.28
    zts

---
+++ F3/Assets/Scripts/ZTS/BtnEffectScripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnEffectScripts : MonoBehaviour
+{
+    public GameObject imgeffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imgeffect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(DeyleShowEffect());
+        }
+    }
+
+    IEnumerator DeyleShowEffect()
+    {
+        imgeffect.transform.position = Input.mousePosition;
+        imgeffect.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        imgeffect.SetActive(false);
+    }
+}
