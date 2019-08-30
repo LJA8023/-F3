387bc84189ab18a23b2ebeb10b091b2e0232f643
赵田胜 <52514396+muchenwusuoweiju@users.noreply.github.com>
2019-08-30 17:07:32 +0800

    8.30
    zts

---
+++ F3/Assets/Scripts/ZTS/CameraLaJin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLaJin : MonoBehaviour
+{
+
+    public Camera camera;
+
+    public float endSize = 0;
+
+    bool isCanLaJin = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCanLaJin = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCanLaJin)
+        {
+            camera.orthographicSize -= Time.deltaTime;
+            if (camera.orthographicSize <= endSize)
+            {
+                isCanLaJin = false;
+                camera.orthographicSize = endSize;
+            }
+        }
+       
+        
+    }
+}
---
+++ F3/Assets/Scripts/ZTS/MoveJiaolianBan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJiaolianBan : MonoBehaviour
+{
+    public float MoveSpeed = 10;
+    bool isCanMoveRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCanMoveRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCanMoveRight)
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * MoveSpeed);
+        }
+        else
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * -MoveSpeed);
+        }
+       
+    }
+}
